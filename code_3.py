72853df027a35fa0571b3bde0095e4383e304e3c
semenk69 <152292047+semenk69@users.noreply.github.com>
2024-04-06 18:09:49 +0300

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,14 @@
+    for item in data:
+def main():
+    return data
+
+
+def generate_random_data():
+
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    data = generate_random_data()
+import random
+    main()
